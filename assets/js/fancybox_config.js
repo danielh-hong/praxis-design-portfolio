6a5c84db788f3648cc88831c6479638d8ba4df21
danielh-hong <danielh.toronto@gmail.com>
2024-04-12 23:13:48 -0400

    Currently Weird Image Double Glitch

--- assets/js/fancybox_config.js
+++ assets/js/fancybox_config.js
@@ -1,1 +1,6 @@
 /* Given up*/
+
+$('.fancybox-button').on('click', function() {
+  $(this).prop('disabled', true);
+  setTimeout(() => $(this).prop('disabled', false), 500); // Re-enable after 500ms
+});
