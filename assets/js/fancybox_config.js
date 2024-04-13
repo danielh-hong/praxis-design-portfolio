e742d0b78628d564e1d4fda5bfc14e83b595b48e
danielh-hong <danielh.toronto@gmail.com>
2024-04-12 23:35:56 -0400

    Could not fix glitch. Reverted All Changes. I'm losing my mind. Help I need to study on exams not o this praxis

--- assets/js/fancybox_config.js
+++ assets/js/fancybox_config.js
@@ -1,6 +1,15 @@
 /* Given up*/
 
-$('.fancybox-button').on('click', function() {
-  $(this).prop('disabled', true);
-  setTimeout(() => $(this).prop('disabled', false), 500); // Re-enable after 500ms
+$('[data-fancybox="images"]').fancybox({
+  idleTime: false,
+  infobar: false,
+  beforeShow: function() {
+      $(".fancybox-caption").addClass('none');
+  },
+  afterShow: function() {
+      $(".fancybox-caption").wrapInner("<span/>");
+      var imageWidth = $(".fancybox-slide--current .fancybox-content").width();
+      $(".fancybox-caption").css("width", imageWidth);
+      setTimeout($(".fancybox-caption").removeClass('none'), 200);
+  }
 });
